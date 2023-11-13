e30590fc9033bb0d3f70893aa3dff1e536568b18
juandejunin <juandejunin75@gmail.com>
2023-11-13 08:09:46 +0100

    feat: download android

--- src/routes/downloadRoutes.js
+++ src/routes/downloadRoutes.js
@@ -1,38 +1,30 @@
 // downloadRoutes.js
 const express = require('express');
+const router = express.Router();
 const path = require('path');
 
-const router = express.Router();
 const downloadController = require('../controller/downloadController');
 
-// Ruta para la descarga
+// Ruta para la descarga real
 router.post('/download', async (req, res) => {
   const videoUrl = req.body.videoUrl;
+  const outputDir = downloadController.getDefaultDownloadDir();
 
   try {
     const cleanTitle = await downloadController.obtenerTituloLimpiado(videoUrl);
-    const outputDir = downloadController.getDefaultDownloadDir();
-
     const fileName = await downloadController.descargarVideo(videoUrl, cleanTitle, outputDir);
 
-    const downloadLink = `/download/${fileName}`;
-    res.send(`Descarga completada. <a href="${downloadLink}">Descargar ${cleanTitle}</a>`);
+    const filePath = path.join(outputDir, fileName);
+    res.download(filePath, fileName, (err) => {
+      if (err) {
+        console.error('Error al enviar el archivo:', err);
+        res.status(500).send('Error al enviar el archivo.');
+      }
+    });
   } catch (error) {
+    console.error('Error en la descarga de video:', error);
     res.status(500).send('Error en la descarga de video.');
   }
-});
-
-// Ruta para la descarga real
-router.get('/download/:fileName', (req, res) => {
-  const fileName = req.params.fileName;
-  const filePath = path.join(downloadController.getDefaultDownloadDir(), fileName);
-
-  res.download(filePath, fileName, (err) => {
-    if (err) {
-      console.error('Error al enviar el archivo:', err);
-      res.status(500).send('Error al enviar el archivo.');
-    }
-  });
 });
 
 module.exports = router;
